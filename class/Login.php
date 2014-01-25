ac05f6bc9e6a8e531190b9663fbbe3b455e9c996
Capestone <capestoneneit@gmail.com>
2014-01-24 21:13:45 -0500

    Update Login.php

--- class/Login.php
+++ class/Login.php
@@ -7,7 +7,7 @@ class Login extends DB {
         $password = sha1($password);
         $db = $this->getDB();
         if ( NULL != $db ) {
-            $stmt = $db->prepare('select * from users where username = :usernameValue and password = :passwordValue limit 1');
+            $stmt = $db->prepare('select * from Users where userName = :usernameValue and password = :passwordValue limit 1');
             $stmt->bindParam(':usernameValue', $username, PDO::PARAM_STR);
             $stmt->bindParam(':passwordValue', $password, PDO::PARAM_STR);
             $stmt->execute();
@@ -23,7 +23,7 @@ class Login extends DB {
         $password = sha1($password);
         $db = $this->getDB();
         if ( NULL != $db ) {
-            $stmt = $db->prepare('select * from users where username = :usernameValue and password = :passwordValue limit 1');
+            $stmt = $db->prepare('select * from Users where userName = :usernameValue and password = :passwordValue limit 1');
             $stmt->bindParam(':usernameValue', $username, PDO::PARAM_STR);
             $stmt->bindParam(':passwordValue', $password, PDO::PARAM_STR);
             $stmt->execute();
