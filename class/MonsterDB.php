405294befd45555618b04a3cf69d759aa829b65b
Capestone <capestoneneit@gmail.com>
2014-01-24 21:13:59 -0500

    Update MonsterDB.php

--- class/MonsterDB.php
+++ class/MonsterDB.php
@@ -6,7 +6,7 @@ class MonsterDB extends DB{
     {
         $db = $this->getDB();
         if ( null != $db ) {
-            $stmt = $db->prepare('select * from monster where monsterID = :monsterIDValue');
+            $stmt = $db->prepare('select * from Monster where monsterID = :monsterIDValue');
             $stmt->bindParam(':monsterIDValue', $monsterID, PDO::PARAM_STR);
             
             if ( $stmt->execute() ) // if everything was excecuted corectly
